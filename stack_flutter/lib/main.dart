a45847939f966137faea0766eea7c020858cb145
CharloLemba <carlolemba98@gmail.com>
2026-09-15 14:46:33 +0800

    update : Overlay Text on Image

--- stack_flutter/lib/main.dart
+++ stack_flutter/lib/main.dart
@@ -12,63 +12,98 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child:
-              // Contoh sederhana Stack
-              /*
-          Stack(
-            alignment: Alignment.center,
-            children: [
-              Container(width: 100, height: 100, color: Colors.blue),
-              Text("I'm On Top!", style: TextStyle(color: Colors.white)),
-            ],
-          ),
-          */
-              // Contoh foto profil dengan indikator online
-              /*
-              Stack(
-                alignment: Alignment.bottomRight,
-                children: [
-                  CircleAvatar(
-                    radius: 40,
-                    backgroundImage: NetworkImage(
-                      "https://avatars.githubusercontent.com/u/79066410?v=4",
+        body: SafeArea(
+          child: Center(
+            child:
+                // Contoh sederhana Stack
+                /*
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Container(width: 100, height: 100, color: Colors.blue),
+                Text("I'm On Top!", style: TextStyle(color: Colors.white)),
+              ],
+            ),
+            */
+                // Contoh foto profil dengan indikator online
+                /*
+                Stack(
+                  alignment: Alignment.bottomRight,
+                  children: [
+                    CircleAvatar(
+                      radius: 40,
+                      backgroundImage: NetworkImage(
+                        "https://avatars.githubusercontent.com/u/79066410?v=4",
+                      ),
                     ),
-                  ),
-                  DecoratedBox(
-                    decoration: BoxDecoration(
-                      color: Colors.lightBlueAccent,
-                      shape: BoxShape.circle,
-                      border: Border.all(color: Colors.white, width: 3),
+                    DecoratedBox(
+                      decoration: BoxDecoration(
+                        color: Colors.lightBlueAccent,
+                        shape: BoxShape.circle,
+                        border: Border.all(color: Colors.white, width: 3),
+                      ),
+                      child: SizedBox(height: 25, width: 25),
                     ),
-                    child: SizedBox(height: 25, width: 25),
-                  ),
-                ],
-              ),
-              */
-              // Contoh gambar product dengan diskon diatas
-              Stack(
-                children: [
-                  Image.network(
-                    "'https://images.pexels.com/photos/19060954/pexels-photo-19060954/free-photo-of-iphone-15-pro-max.jpeg'",
-                  ),
-                  Positioned(
-                    top: 10,
-                    right: 10,
-                    child: Container(
-                      padding: EdgeInsets.all(8),
-                      color: Colors.red,
-                      child: Text(
-                        "Diskon 20%",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
+                  ],
+                ),
+                */
+                // Contoh gambar product dengan diskon diatas
+                /*
+                Stack(
+                  children: [
+                    Image.network(
+                      "https://images.pexels.com/photos/19060954/pexels-photo-19060954/free-photo-of-iphone-15-pro-max.jpeg",
+                    ),
+                    Positioned(
+                      top: 10,
+                      right: 10,
+                      child: Container(
+                        padding: EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.white, width: 1),
+                          color: Colors.red,
+                        ),
+                        child: Text(
+                          "Diskon 20%",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
+                  ],
+                ),
+                */
+                // Overlay Text on Image
+                Stack(
+                  children: [
+                    Image.network(
+                      "https://images.pexels.com/photos/19060954/pexels-photo-19060954/free-photo-of-iphone-15-pro-max.jpeg",
+                    ),
+                    Positioned(
+                      bottom: 10,
+                      left: 10,
+                      child: DecoratedBox(
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                          borderRadius: BorderRadius.circular(4),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.all(8),
+                          child: Text(
+                            "iPhone 15 Pro Max",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+          ),
         ),
       ),
     );
